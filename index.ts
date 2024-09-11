02abfc2ed52e440d8317d1facdaf84b23479744a
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-09-11 08:39:23 +0530

    NEP

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ const app = express();
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server ready on port ${port}.`));
 
 module.exports = app;
